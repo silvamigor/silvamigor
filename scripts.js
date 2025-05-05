5a92efc112c9e7ddf9bd2f05f05828a08b3109c6
silvamigor@gmail.com <silvamigor@gmail.com>
2025-05-05 17:29:17 -0300

    probabilidade

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-export function closeDetail(el) {
+function closeDetail(el) {
     el.open = false;  // mais legível
     el.scrollIntoView({ block: "nearest", behavior: "smooth" });
     history.replaceState(null, '', '#' + el.id);  // altera só o hash sem recarregar
